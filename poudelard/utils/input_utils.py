ef20679c2779a2c7a3b670877095edce58337706
mathieu_sgp <mathieu.singpraseuth@efrei.net>
2025-12-09 13:07:59 +0100

    commit3

---
+++ poudelard/utils/input_utils.py
@@ -0,0 +1,12 @@
+"""demander_texte(message)
+Cette fonction doit être appelée chaque fois qu’on demande à l’utilisateur de saisir du texte. Elle
+vérifie que la saisie (le texte passé en paramètre) n’est pas vide. Si l’utilisateur ne saisit rien ou
+seulement des espaces, la fonction redemande la saisie jusqu’à obtenir un texte valide. Elle
+retourne la saisie texte de l’utilisateur.
+Astuce du grimoire : pour gérer correctement les saisies, n’oubliez pas de supprimer les espaces
+en début et fin de chaîne (par exemple en utilisant la méthode strip()).
+Exemple :
+nom = demander_texte("Entrez le nom de votre personnage : ")
+Sortie :
+Entrez le nom de votre personnage : Potter"""
+
