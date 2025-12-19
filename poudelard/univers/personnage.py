ff43c6e45a400d63ce8762a0e89decff2ad451ea
Stali <stefen.stalin@efrei.net>
2025-12-19 18:56:53 +0100

    afficher

--- poudelard/univers/personnage.py
+++ poudelard/univers/personnage.py
@@ -1,11 +1,34 @@
 def initialiser_personnage(nom, prenom, attributs):
+    liste_inventaire = []
+    liste_sortilege = []
+    argent = 100
     joueur = {
         "Nom": nom,
-        "Prénom": prenom,
-        "Argent": 100,
-        "Inventaire": [],
-        "Sortilèges": [],
+        "Prenom": prenom,
+        "Argent": argent,
+        "Inventaire": liste_inventaire,
+        "Sortilèges": liste_sortilege,
         "Attributs": attributs
     }
 
     return joueur
+
+def afficher_personnage(joueur):
+    print("Profil du personnage :")
+
+    for cle in joueur:
+        valeur = joueur[cle]
+
+        if isinstance(valeur, dict):
+            print(f"{cle} :")
+            for sous_cle in valeur:
+                print(f"- {sous_cle} : {valeur[sous_cle]}")
+
+        elif isinstance(valeur, list):
+            if valeur:
+                print(f"{cle} : {', '.join(valeur)}")
+            else:
+                print(f"{cle} :")
+
+        else:
+            print(f"{cle} : {valeur}")
